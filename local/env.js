f5a06fc9d124597a4e78c4846136a48ca3988bc7
tanpike <64754346+tanpike@users.noreply.github.com>
2024-01-21 10:40:22 -0600

    Update env.js

--- local/env.js
+++ local/env.js
@@ -1,3 +1,3 @@
 //var backendURL = "https://gall.tanpike.repl.co";
-var backendURL = "https://828752ab-e444-452b-b031-dbbb6ed20c47-00-2ym6382kxgwkc.global.replit.dev";
+var backendURL = "https://828752ab-e444-452b-b031-dbbb6ed20c47-00-1x6il4kqn2zum.kirk.replit.dev";
 
